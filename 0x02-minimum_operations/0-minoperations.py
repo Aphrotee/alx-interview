1ce9dc5fccb78cb14a159e5cc9592332486615d7
Temitope Aiyebogan <aphrotemitope37@gmail.com>
2023-01-11 02:22:30 +0100

    completed and tested

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+
+'''
+This module provides the function minOpertions
+'''
+
+
+def minOperations(n: int) -> int:
+    '''
+    This is a function that calculates the fewest number
+    of operations needed to result in exactly n H characters in the file.
+    '''
+    if n < 1 or type(n) != int:
+        return 0
+    operations: int = 0
+    while n >= 2:
+        i: int = 2
+        while i < n + 1:
+            if n % i == 0:
+                operations += i
+                n /= i
+                break
+            i += 1
+    return operations
