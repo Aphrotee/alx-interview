72983443226b831e1c53811b09cd3eeba576b8b0
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-01-16 00:07:55 +0100

    PEP 8 styling

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,10 +13,10 @@ def minOperations(n: int) -> int:
     if n < 1 or type(n) != int:
         # Checks if the input is not a positive integer
         return 0
-    operations: int = 0 # number of operations counter
+    operations: int = 0  # number of operations counter
     while n >= 2:
         # number of H characters from all operations cannot be less than 2
-        lowestFactor: int = 2 # initialized at 2 which is 
+        lowestFactor: int = 2  # initialized at 2 which is
         # the lowest possible factor asides 1
         while lowestFactor < n + 1:
             # loops to find the lowest factor of n
@@ -24,9 +24,10 @@ def minOperations(n: int) -> int:
                 # when the lowest factor is found it's the number of operations
                 # performed to arrive at the current n number of H characters
                 # i.e 1 `copyAll`, lowestFactor - 1 `paste`
-                operations += lowestFactor # increment the number of operations
-                n /= lowestFactor # divide the current n by lowestFactor to
+                operations += lowestFactor  # increment the number of operation
+                n /= lowestFactor  # divide the current n by lowestFactor to
                 # get the result of all operations prior
-                break # repeat the whole process
-            lowestFactor += 1 # increments to keep searching for the lowest factor
-    return operations # returns the minimum number of operations
+                break  # repeat the whole process
+            lowestFactor += 1  # increments to keep
+            # searching for the lowest factor
+    return operations  # returns the minimum number of operations
