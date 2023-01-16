176c8f0bfad41849622d9aaa247219404f82a011
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2023-01-16 01:01:08 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This provides the algo function minOpertions
+This module provides the function `minOpertions`
 """
 
 
@@ -13,10 +13,10 @@ def minOperations(n):
     if n < 1 or type(n) != int:
         # Checks if the input is not a positive integer
         return 0
-    operations: int = 0  # number of operations counter
+    operations = 0 # number of operations counter
     while n >= 2:
         # number of H characters from all operations cannot be less than 2
-        lowestFactor: int = 2  # initialized at 2 which is
+        lowestFactor = 2 # initialized at 2 which is 
         # the lowest possible factor asides 1
         while lowestFactor < n + 1:
             # loops to find the lowest factor of n
@@ -24,10 +24,9 @@ def minOperations(n):
                 # when the lowest factor is found it's the number of operations
                 # performed to arrive at the current n number of H characters
                 # i.e 1 `copyAll`, lowestFactor - 1 `paste`
-                operations += lowestFactor  # increment the number of operation
-                n /= lowestFactor  # divide the current n by lowestFactor to
+                operations += lowestFactor # increment the number of operations
+                n /= lowestFactor # divide the current n by lowestFactor to
                 # get the result of all operations prior
-                break  # repeat the whole process
-            lowestFactor += 1  # increments to keep
-            # searching for the lowest factor
-    return operations  # returns the minimum number of operations
+                break # repeat the whole process
+            lowestFactor += 1 # increments to keep searching for the lowest factor
+    return operations # returns the minimum number of operations
