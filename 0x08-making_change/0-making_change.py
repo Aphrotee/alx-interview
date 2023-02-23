3b0986d2305c6ce9253ca73f0cb37092a936c72e
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2023-02-23 19:16:20 +0100

    edit solution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,26 +6,22 @@ This module provides the `makeChange` function
 
 
 def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
-    This function returns fewest number of coins needed to meet total
-    """
-    if coins is None or total is None:
-        return -1
-    if not isinstance(total, int):
-        return 0
     if total <= 0:
         return 0
-    if len(coins) <= 0:
-        return 0
-    count, totalCount = 0, 0
-    i = len(coins) - 1
-    coins.sort()
-    while total > 0:
-        if i < 0:
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
             return -1
-        if coins[i] < total:
-            count = total // coins[i]
-            totalCount += count
-            total -= (count * coins[i])
-        i -= 1
-    return totalCount
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
